a814cb8615ab110c261ae14a669b02373d8c6c8d
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2025-01-19 10:55:33 +0500

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const input = document.querySelector('.input');
 const btn = document.getElementById('btn');
 const spam = document.querySelector('.spam');
+const btn4 = document.querySelector('.btn-4');
 
 
-btn.addEventListener('click' , ()=>{
-     spam.textContent = input.value;  
-});
+btn.addEventListener('click' , (event)=>{
+     const date = input.value.trim();
+    if(date){
+     const listen = document.createElement('ul');
+     listen.textContent = date;
+     spam.appendChild(listen);
+
+
+     input.value = ' ';
+    }else{
+       alert('Iltimos malumot kiriting!!')
+    }
+ });
+
+ 
